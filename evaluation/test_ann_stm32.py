baffb08f65744d6f5ff5bd184ab198c0624592f1
JochiSt <jochen.steinmann@web.de>
2022-03-15 12:24:35 +0100

    added template generation and plot of used and received template

--- evaluation/test_ann_stm32.py
+++ evaluation/test_ann_stm32.py
@@ -1,14 +1,12 @@
 """
-    script for reading the PeakTech DMM 3315
 
-    inspired by pyUSBtin
 """
-
 import serial
 import numpy as np
+import matplotlib.pyplot as plt
+from generateTemplate import generateTemplate
 
 def main():
-
     serial_port = None
     try:
         # open serial port
@@ -35,9 +33,18 @@ def main():
     # some debug info
     print("connected to AI test")
 
-    test_data = np.arange(0,128, dtype=np.uint8)
-    print(len(test_data))
-    print(test_data.tobytes())
+    # create a new waveform
+    width = np.random.randint(16, 64)                    # width of 16 to 64
+    position = np.random.randint(0, high=96-width) # arbitrary position
+    noise = np.random.randint(1,30)/10.                 # 0.1 to 3 LSB noise
+    height = np.random.randint(32,128)                  # pulse height of 32 to 128
+
+    noise = 0.
+
+    print(width, position, height)
+
+    test_data = generateTemplate(position = position, pulse_length = width, height=height, noise=noise)
+    test_data = test_data.astype(np.uint8)
 
     try:
         serial_port.write( test_data.tobytes() )
@@ -48,17 +55,25 @@ def main():
 
     try:
         raw = serial_port.readline()
-        print(len(raw))
-        print(raw)
+        raw = raw.decode("utf-8")[:-1]
+        STM32waveform = np.array(raw.split(" "))
+
         raw = serial_port.readline()
-        print(len(raw))
-        print(raw)
+        raw = raw.decode("utf-8")[:-1]
+
+        width, position, height = raw.split("\t")
+
+        print( width, position, height )
     except Exception as e:
         print(e)
         serial_port.close()
         return
 
     serial_port.close()
+
+    plt.plot( STM32waveform, 'b.')
+    plt.plot( test_data, 'r.')
+    plt.show()
 
 if __name__ == "__main__":
     main()
