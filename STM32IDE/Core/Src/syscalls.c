ec2789dc995ffb17a2a0fae8b2eba6bd87a247b9
JochiSt <jochen.steinmann@web.de>
2022-03-15 10:57:33 +0100

    adapted to UART 2

--- STM32IDE/Core/Src/syscalls.c
+++ STM32IDE/Core/Src/syscalls.c
@@ -2,7 +2,7 @@
 #include  <errno.h>
 #include  <sys/unistd.h>		// STDOUT_FILENO, STDERR_FILENO
 
-char ENABLE_UART_DEBUG=1;		//< should the debug output (printf) been printed
+char ENABLE_UART_DEBUG=2;		//< should the debug output (printf) been printed
 
 int _write(int file, char *data, int len){
    if ((file != STDOUT_FILENO) && (file != STDERR_FILENO)){
