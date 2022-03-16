4dd1742ecfaa995fb6d0c1067f925368dbc64d84
JochiSt <jochen.steinmann@web.de>
2022-03-16 14:56:45 +0100

    do many test runs and export data into npz

--- evaluation/test_ann_stm32.py
+++ evaluation/test_ann_stm32.py
@@ -1,9 +1,14 @@
 """
 
 """
+
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+import time
+
 from generateTemplate import generateTemplate
 
 def main():
@@ -33,48 +38,101 @@ def main():
     # some debug info
     print("connected to AI test")
 
-    # create a new waveform
-    width = np.random.randint(16, 64)                    # width of 16 to 64
-    position = np.random.randint(0, high=96-width) # arbitrary position
-    noise = np.random.randint(1,30)/10.                 # 0.1 to 3 LSB noise
-    height = np.random.randint(32,128)                  # pulse height of 32 to 128
+    gene_params = []
+    reco_params = []
 
-    noise = 0.
+    TEST_SIZE = 10000
+    for i in range(TEST_SIZE):
+        if i % 10 == 0:
+            print(i,"of", TEST_SIZE)
 
-    print(width, position, height)
+        # create a new waveform
 
-    test_data = generateTemplate(position = position, pulse_length = width, height=height, noise=noise)
-    test_data = test_data.astype(np.uint8)
+        # parameters for template generation
+        P_width    = ( 16,  32 )
+        P_position = ( 16,  24 )
+        P_height   = ( 32, 128 )
 
-    try:
-        serial_port.write( test_data.tobytes() )
-    except Exception as e:
-        print(e)
-        serial_port.close()
-        return
+        width    = np.random.randint( *P_width )       # width of 16 to 64
+        position = np.random.randint( *P_position ) # arbitrary position
+        height   = np.random.randint( *P_height )     # pulse height of 32 to 128
 
-    try:
-        raw = serial_port.readline()
-        #print(raw)
-        raw = raw.decode("utf-8")[:-1]
-        STM32waveform = np.array(raw.split(" "))
+        noise = np.random.randint(1,30)/10.                 # 0.1 to 3 LSB noise
 
-        raw = serial_port.readline()
-        #print(raw)
-        raw = raw.decode("utf-8")[:-1]
+        noise = 0.
 
-        width, position, height = raw.split("\t")[:-1]  # we have one trailing tab
+        #print(width, position, height)
+        # scale parameters to get values between 0 and 1
+        SCALE_WIDTH = 32
+        SCALE_POS = 32
+        SCALE_HEIGHT = 255
 
-        print( width, position, height )
-    except Exception as e:
-        print(e)
-        serial_port.close()
-        return
+        gene_params.append( (width, position, height) )
+
+        test_data = generateTemplate(position = position, pulse_length = width, height=height, noise=noise)
+        test_data = test_data.astype(np.uint8)
+
+        try:
+            serial_port.write( test_data.tobytes() )
+        except Exception as e:
+            print(e)
+            serial_port.close()
+            break
+
+        try:
+            # readin waveform send to STM32 (it's echoed)
+            raw = serial_port.readline()
+            #print(raw)
+            raw = raw.decode("utf-8")[:-1]
+            STM32waveform = np.array(raw.split(" "))
+
+            # float output
+            #raw = serial_port.readline()
+            #raw = raw.decode("utf-8")[:-1]
+            #print(raw)
+
+            # read reconstructed parameters
+            raw = serial_port.readline()
+            #print(raw)
+            raw = raw.decode("utf-8")[:-1]
+            width, position, height = raw.split("\t")[:-1]  # we have one trailing tab
+
+            #print( width, position, height )
+            reco_params.append( (
+                    float(width)    * SCALE_WIDTH,
+                    float(position) * SCALE_POS,
+                    float(height)   * SCALE_HEIGHT
+                    ) )
+        except Exception as e:
+            print(e)
+            serial_port.close()
+            break
+
+    np.savez("stm32_ann_evaluation_%s.npz"%(time.strftime("%Y%m%d_%H%M%S")),
+             reco_params=reco_params,
+             gene_params=gene_params
+             )
 
     serial_port.close()
+    plt.title("width")
+    plt.ylabel("STM32 parameter")
+    plt.xlabel("generated parameter")
+    plt.hist2d( [ float(i[0]) for i in gene_params ], [ float(i[0]) for i in reco_params ], bins=32)
+    plt.savefig("stm32_validation_width.png")
+    plt.show()
 
-    plt.plot( STM32waveform, 'b.')
-    plt.plot( test_data, 'r.')
+    plt.title("position")
+    plt.ylabel("STM32 parameter")
+    plt.xlabel("generated parameter")
+    plt.hist2d( [ float(i[1]) for i in gene_params ], [ float(i[1]) for i in reco_params ], bins=32)
+    plt.savefig("stm32_validation_position.png")
+    plt.show()
+
+    plt.title("height")
+    plt.ylabel("STM32 parameter")
+    plt.xlabel("generated parameter")
+    plt.hist2d( [ float(i[2]) for i in gene_params ], [ float(i[2]) for i in reco_params ], bins=32)
+    plt.savefig("stm32_validation_height.png")
     plt.show()
 
 if __name__ == "__main__":
