9cead16cd7de9e1ac0fcb551c81e4a10835e50e9
JochiSt <jochen.steinmann@web.de>
2022-03-17 15:13:37 +0100

    optimised binning
    bins are now 1/12MHz wide

--- evaluation/timing/analyse_pulse_width.py
+++ evaluation/timing/analyse_pulse_width.py
@@ -50,23 +50,40 @@ def analyse_pulse_width(vcd_files):
     std_duration = np.std(duration)
 
     fig, ax = plt.subplots()
-    biny, binx, _ = plt.hist(duration, bins=100, range=(mean_duration-5*std_duration, mean_duration+5*std_duration) )
+
+    hist_range = [mean_duration-5*std_duration, mean_duration+5*std_duration]
+    delta_hist_range = hist_range[1]-hist_range[0]
+    hist_bins = int( np.round( delta_hist_range*1e-6 / (1. / 12E6), 0))    # assume 12MHz sampling
+    bin_width = delta_hist_range / hist_bins * 1000
+
+    """
+    print("Histogram range:", np.round(delta_hist_range, 2), "us")
+    print("Bins:", hist_bins)
+    print("bin width", np.round(bin_width, 2) , "ns")
+    print("bin frequency", np.round( 1/(bin_width*1e-9) / 1e6, 3) , "MHz")
+    """
+
+    biny, binx, _ = plt.hist(duration, bins=hist_bins,
+                             range=hist_range )
 
     x = np.linspace(binx.min(), binx.max(), 100)
-    plt.plot(x, stats.norm.pdf(x, mean_duration, std_duration)*biny.max() )
+    plt.plot(x, stats.norm.pdf(x, mean_duration, std_duration) * np.max(biny) )
 
     plt.text( 0.05, 0.90,  r'N = %d'%(len(duration)), ha='left', va='center', transform=ax.transAxes )
-    plt.text( 0.05, 0.84,  r'$\Delta T =%6.2f \pm %6.2f \, \mu s$'%(mean_duration, std_duration), ha='left', va='center', transform=ax.transAxes )
+    plt.text( 0.05, 0.84,  r'$\Delta T =%6.2f \pm %6.2f \, \mu \mathrm{s}$'%(mean_duration, std_duration), ha='left', va='center', transform=ax.transAxes )
+
+    plt.text( 0.95, 0.90,  r'$\Delta_\mathrm{bin} =%4.2f \, \mathrm{ns}$'%(bin_width), ha='right', va='center', transform=ax.transAxes )
 
     mean_freq = 1/(mean_duration*1e-6)
     std_freq     = 1/(mean_duration*1e-6)**2 * std_duration*1e-6
 
-    plt.text( 0.05, 0.78,  r'$f =%6.2f \pm %6.2f \, Hz$'%(mean_freq, std_freq), ha='left', va='center', transform=ax.transAxes )
+    plt.text( 0.05, 0.78,  r'$f =%6.2f \pm %6.2f \, \mathrm{Hz}$'%(mean_freq, std_freq), ha='left', va='center', transform=ax.transAxes )
 
     plt.title("ANN runtime on STM32")
     plt.ylabel("entries")
     plt.xlabel("ANN execution time / us")
     plt.savefig("ANNexecutionTime.png")
+    plt.tight_layout()
     plt.show()
 
 
